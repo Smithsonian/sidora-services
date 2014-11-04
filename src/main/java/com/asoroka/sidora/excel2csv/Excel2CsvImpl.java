62397645bd1becd74fc8e6e595bfe048a07374d7
ajs6f <ajs6f@virginia.edu>
2014-11-04 15:37:40 -0500

    Cleaning out unneeded helper methods

--- src/main/java/com/asoroka/sidora/excel2csv/Excel2CsvImpl.java
+++ src/main/java/com/asoroka/sidora/excel2csv/Excel2CsvImpl.java
@@ -2,16 +2,12 @@
 package com.asoroka.sidora.excel2csv;
 
 import static com.google.common.base.Throwables.propagate;
-import static java.util.UUID.randomUUID;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
 import java.net.URL;
-import java.nio.file.Files;
 import java.util.List;
 
 import org.apache.poi.hssf.eventusermodel.FormatTrackingHSSFListener;
@@ -47,46 +43,28 @@ public class Excel2CsvImpl implements Excel2Csv {
     @Override
     public List<File> apply(final URL inputUrl) {
 
-        final File tmpFile = createTempFile();
-
         final POIFSFileSystem poiFileSystem;
         try (final InputStream inputStream = inputUrl.openStream()) {
             poiFileSystem = new POIFSFileSystem(inputStream);
 
-            try (final PrintStream outputStream = new PrintStream(tmpFile)) {
+            final XLS2CSVmra poiTransformer =
+                    new XLS2CSVmra(poiFileSystem, minColumns);
+            final MissingRecordAwareHSSFListener listener = new MissingRecordAwareHSSFListener(
+                    poiTransformer);
+            final FormatTrackingHSSFListener ftListener = new FormatTrackingHSSFListener(
+                    listener);
 
-                final XLS2CSVmra poiTransformer =
-                        new XLS2CSVmra(poiFileSystem, minColumns);
-                final MissingRecordAwareHSSFListener listener = new MissingRecordAwareHSSFListener(
-                        poiTransformer);
-                final FormatTrackingHSSFListener ftListener = new FormatTrackingHSSFListener(
-                        listener);
+            poiTransformer.setFormatListener(ftListener);
 
-                poiTransformer.setFormatListener(ftListener);
-
-                final HSSFRequest request = new HSSFRequest();
-                request.addListenerForAllRecords(ftListener);
-                final HSSFEventFactory factory = new HSSFEventFactory();
-                factory.processWorkbookEvents(request, poiFileSystem);
-                return poiTransformer.getOutputs();
-            } catch (final FileNotFoundException e) {
-                log.error("Could not open self-created temp file!");
-                throw new AssertionError(e);
-            }
+            final HSSFRequest request = new HSSFRequest();
+            request.addListenerForAllRecords(ftListener);
+            final HSSFEventFactory factory = new HSSFEventFactory();
+            factory.processWorkbookEvents(request, poiFileSystem);
+            return poiTransformer.getOutputs();
 
         } catch (final IOException e) {
-            log.error("Could not read input URL: {}!", inputUrl);
+            log.error("Could not process input URL: {}!", inputUrl);
             throw propagate(e);
         }
     }
-
-    private static File createTempFile() {
-        try {
-            return Files.createTempFile(Excel2CsvImpl.class.getName(), randomUUID().toString()).toFile();
-        } catch (final IOException e) {
-            log.error("Could not create temp file!");
-            throw new AssertionError(e);
-        }
-    }
-
 }
