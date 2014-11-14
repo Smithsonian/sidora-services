2c6db228a07604e7ced07c505a8966f180c7a85d
ocymum <ocymum@gmail.com>
2014-11-14 08:06:59 -0500

    Simple theory for min/max test

--- tabular-metadata-generator/src/test/java/com/asoroka/sidora/tabularmetadata/heuristics/ranges/RunningMinMaxHeuristicTest.java
+++ tabular-metadata-generator/src/test/java/com/asoroka/sidora/tabularmetadata/heuristics/ranges/RunningMinMaxHeuristicTest.java
@@ -4,17 +4,29 @@ package com.asoroka.sidora.tabularmetadata.heuristics.ranges;
 import static com.google.common.collect.ImmutableMap.of;
 import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Range.closed;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import static java.util.Arrays.asList;
 import static org.joda.time.format.ISODateTimeFormat.dateTimeParser;
 import static org.junit.Assert.assertEquals;
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.lang.annotation.Retention;
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import org.joda.time.DateTime;
 import org.junit.Test;
+import org.junit.experimental.theories.ParameterSignature;
+import org.junit.experimental.theories.ParameterSupplier;
+import org.junit.experimental.theories.ParametersSuppliedBy;
+import org.junit.experimental.theories.PotentialAssignment;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 
 import com.asoroka.sidora.tabularmetadata.datatype.DataType;
@@ -31,6 +43,7 @@ import com.google.common.collect.Range;
  * @author ajs6f
  */
 @SuppressWarnings("rawtypes")
+@RunWith(Theories.class)
 public class RunningMinMaxHeuristicTest extends HeuristicTestFrame<RunningMinMaxHeuristic> {
 
     private static Map<DataType, Map<Range, List>> sampleData;
@@ -122,5 +135,66 @@ public class RunningMinMaxHeuristicTest extends HeuristicTestFrame<RunningMinMax
         final Range<GeographicValue> geographicRange =
                 (Range<GeographicValue>) testStrategy.getRanges().get(DataType.Geographic);
         assertEquals("Found a defined range where we should not have!", Range.all(), geographicRange);
+    }
+
+    @Theory
+    public void testIntegerRanges(@ParametersSuppliedBy(BetweenSupplier.class) @Between(from = -5,
+            through = 5) @Type(Integer.class) final Integer from,
+            @ParametersSuppliedBy(BetweenSupplier.class) @Between(from = -5,
+                    through = 5) @Type(Integer.class) final Integer through) {
+        final RunningMinMaxHeuristic testStrategy = newTestHeuristic();
+        final Integer trueMin = min(through, from);
+        final Integer trueMax = max(from, through);
+
+        for (int i = trueMin; i <= trueMax; i++) {
+            testStrategy.addValue(String.valueOf(i));
+        }
+
+        final Integer calculatedMax = (Integer) testStrategy.getRanges().get(DataType.Integer).upperEndpoint();
+        final Integer calculatedMin = (Integer) testStrategy.getRanges().get(DataType.Integer).lowerEndpoint();
+        assertEquals("Calculated maximum should be the true maximum!", trueMax, calculatedMax);
+        assertEquals("Calculated minimum should be the true minimum!", trueMin, calculatedMin);
+    }
+
+    @Retention(RUNTIME)
+    public @interface Type {
+
+        Class<?> value();
+    }
+
+    @Retention(RUNTIME)
+    public @interface Between {
+
+        int from();
+
+        int through();
+    }
+
+    public static class BetweenSupplier<T> extends ParameterSupplier {
+
+        @Override
+        public List<PotentialAssignment> getValueSources(final ParameterSignature sig) {
+            final List<PotentialAssignment> list = new ArrayList<>();
+            final Between range = sig.getAnnotation(Between.class);
+            final Class<?> type = sig.getAnnotation(Type.class).value();
+
+            for (int i = range.from(); i <= range.through(); i++) {
+                final T value = (T) type.cast(i);
+                list.add(new PotentialAssignment() {
+
+                    @Override
+                    public T getValue() {
+                        return value;
+                    }
+
+                    @Override
+                    public String getDescription() {
+                        return null;
+                    }
+                });
+
+            }
+            return list;
+        }
     }
 }
