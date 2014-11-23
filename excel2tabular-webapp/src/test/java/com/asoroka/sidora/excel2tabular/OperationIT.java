e564dfc3bc850da70f71390fc1ef1a588c46e6b1
ajs6f <ajs6f@virginia.edu>
2014-11-23 17:17:32 -0500

    Stronger integration test

--- excel2tabular-webapp/src/test/java/com/asoroka/sidora/excel2tabular/OperationIT.java
+++ excel2tabular-webapp/src/test/java/com/asoroka/sidora/excel2tabular/OperationIT.java
@@ -4,6 +4,7 @@ package com.asoroka.sidora.excel2tabular;
 import static com.google.common.io.ByteStreams.toByteArray;
 import static javax.ws.rs.core.Response.Status.OK;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.File;
@@ -29,6 +30,16 @@ public class OperationIT {
 
     private static final Logger log = getLogger(OperationIT.class);
 
+    private static String correctMimeType = "Content-Type: text/csv";
+
+    private static String correctSheetId = "Content-ID: <Sheet:1>";
+
+    private static String correctContent =
+            "\"Animal counts\",\"Animal color\",\"Animal size\",\"IsMammal\",\"Confused calculation\"\n" +
+                    "1,\"Red\",\"Big\",true,#VALUE!\n" +
+                    "3,\"Blue\",\"Little\",false,#VALUE!\n" +
+                    "4,\"Green\",\"Tiny\",true,#VALUE!";
+
     @Test
     public void testUrlOperation() throws IOException {
         final File dataFileLocation = new File("src/test/resources/small-test.xls");
@@ -36,6 +47,8 @@ public class OperationIT {
         assertEquals(OK.getStatusCode(), r.getStatus());
         final String text = new String(toByteArray((InputStream) r.getEntity()));
         log.trace("Received {}", text);
+        assertTrue(text.contains(correctMimeType));
+        assertTrue(text.contains(correctSheetId));
+        assertTrue(text.contains(correctContent));
     }
-
 }
