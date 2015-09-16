a6983175ccbb84183381d79c09b7fd513c65ca08
ajs6f <ajs6f@virginia.edu>
2015-09-16 10:47:33 -0400

    Adding appropriate license

--- tabular-metadata-spring/src/test/java/com/asoroka/sidora/tabularmetadata/spring/SpringITFramework.java
+++ tabular-metadata-spring/src/test/java/com/asoroka/sidora/tabularmetadata/spring/SpringITFramework.java
@@ -1,3 +1,30 @@
+/**
+ * Copyright 2015 Smithsonian Institution.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.You may obtain a copy of
+ * the License at: http://www.apache.org/licenses/
+ *
+ * This software and accompanying documentation is supplied without
+ * warranty of any kind. The copyright holder and the Smithsonian Institution:
+ * (1) expressly disclaim any warranties, express or implied, including but not
+ * limited to any implied warranties of merchantability, fitness for a
+ * particular purpose, title or non-infringement; (2) do not assume any legal
+ * liability or responsibility for the accuracy, completeness, or usefulness of
+ * the software; (3) do not represent that use of the software would not
+ * infringe privately owned rights; (4) do not warrant that the software
+ * is error-free or will be maintained, supported, updated or enhanced;
+ * (5) will not be liable for any indirect, incidental, consequential special
+ * or punitive damages of any kind or nature, including but not limited to lost
+ * profits or loss of data, on any basis arising from contract, tort or
+ * otherwise, even if any of the parties has been warned of the possibility of
+ * such loss or damage.
+ *
+ * This distribution includes several third-party libraries, each with their own
+ * license terms. For a complete copy of all copyright and license terms, including
+ * those of third-party libraries, please see the product release notes.
+ */
+
 
 package com.asoroka.sidora.tabularmetadata.spring;
 
@@ -6,8 +33,6 @@ import static com.asoroka.sidora.tabularmetadata.datatype.DataType.PositiveInteg
 import static com.asoroka.sidora.tabularmetadata.datatype.DataType.String;
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
-import static org.slf4j.LoggerFactory.getLogger;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -17,7 +42,6 @@ import java.util.List;
 import javax.inject.Inject;
 
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.asoroka.sidora.tabularmetadata.TabularMetadata;
@@ -32,8 +56,6 @@ import com.google.common.collect.Range;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 public abstract class SpringITFramework {
-
-    private static final Logger log = getLogger(SpringITFramework.class);
 
     protected static final File testDataDir = new File("src/test/resources/test-data");
 
