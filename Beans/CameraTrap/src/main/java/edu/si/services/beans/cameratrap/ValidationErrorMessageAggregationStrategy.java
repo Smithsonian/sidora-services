9bb77c89cce41285b14d973f70ff6e1c478b6580
jbirkhimer <jbirkhimer@quotient-inc.com>
2016-03-16 10:36:44 -0400

    [SID-569] EAC-CPF, FGDC, MODS, and aggregation of error messages complete.

---
+++ Beans/CameraTrap/src/main/java/edu/si/services/beans/cameratrap/ValidationErrorMessageAggregationStrategy.java
@@ -0,0 +1,33 @@
+package edu.si.services.beans.cameratrap;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by jbirkhimer on 3/10/16.
+ */
+public class ValidationErrorMessageAggregationStrategy implements AggregationStrategy{
+
+    final Logger log = LoggerFactory.getLogger(PostValidationBean.class);
+
+    public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
+
+        if (oldExchange == null) {
+            return newExchange;
+        }
+
+        String oldBody = oldExchange.getIn().getBody(String.class);
+        String newBody = newExchange.getIn().getBody(String.class);
+
+        //Filter out the passed validation messages
+        if (!oldBody.equals("passed") && !newBody.equals("passed")) {
+            oldExchange.getIn().setBody(oldBody + ", " + newBody);
+        } else if (oldBody.equals("passed") && !newBody.equals("passed")) {
+            oldExchange.getIn().setBody(newBody);
+        }
+
+        return oldExchange;
+    }
+}
