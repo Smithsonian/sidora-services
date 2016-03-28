d44b1caf23bd73591fe8beefe65308f20d91d993
John Park <parkj@si.edu>
2016-03-28 14:53:52 -0400

    Updated SI Copyright text to include "2015-2016"

--- Tabular/tabular-metadata-generator/src/main/java/edu/si/sidora/tabularmetadata/heuristics/enumerations/package-info.java
+++ Tabular/tabular-metadata-generator/src/main/java/edu/si/sidora/tabularmetadata/heuristics/enumerations/package-info.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2015 Smithsonian Institution.
+/*
+ * Copyright 2015-2016 Smithsonian Institution.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.You may obtain a copy of
