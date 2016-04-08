c2e1c2129473e34518f01c07ba7951b7d186ce9e
jbirkhimer <jbirkhimer@quotient-inc.com>
2016-04-08 10:09:43 -0400

    [SID-545] updated to reject archives with directories. Added an Exception class. Added some tests and test resources.

---
+++ Components/Extractor/src/main/java/edu/si/services/camel/extractor/ExtractorException.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2015-2016 Smithsonian Institution.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.You may obtain a copy of
+ * the License at: http://www.apache.org/licenses/
+ *
+ * This software and accompanying documentation is supplied without
+ * warranty of any kind. The copyright holder and the Smithsonian Institution:
+ * (1) expressly disclaim any warranties, express or implied, including but not
+ * limited to any implied warranties of merchantability, fitness for a
+ * particular purpose, title or non-infringement; (2) do not assume any legal
+ * liability or responsibility for the accuracy, completeness, or usefulness of
+ * the software; (3) do not represent that use of the software would not
+ * infringe privately owned rights; (4) do not warrant that the software
+ * is error-free or will be maintained, supported, updated or enhanced;
+ * (5) will not be liable for any indirect, incidental, consequential special
+ * or punitive damages of any kind or nature, including but not limited to lost
+ * profits or loss of data, on any basis arising from contract, tort or
+ * otherwise, even if any of the parties has been warned of the possibility of
+ * such loss or damage.
+ *
+ * This distribution includes several third-party libraries, each with their own
+ * license terms. For a complete copy of all copyright and license terms, including
+ * those of third-party libraries, please see the product release notes.
+ */
+
+package edu.si.services.camel.extractor;
+
+/**
+ * @author jbirkhimer
+ */
+public class ExtractorException extends Exception {
+    /**
+     * Constructs a new exception with {@code null} as its detail message.
+     * The cause is not initialized, and may subsequently be initialized by a
+     * call to {@link #initCause}.
+     */
+    public ExtractorException() {
+        super();
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message.  The
+     * cause is not initialized, and may subsequently be initialized by
+     * a call to {@link #initCause}.
+     *
+     * @param   message   the detail message. The detail message is saved for
+     *          later retrieval by the {@link #getMessage()} method.
+     */
+    public ExtractorException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new exception with the specified detail message and
+     * cause.  <p>Note that the detail message associated with
+     * {@code cause} is <i>not</i> automatically incorporated in
+     * this exception's detail message.
+     *
+     * @param  message the detail message (which is saved for later retrieval
+     *         by the {@link #getMessage()} method).
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A <tt>null</tt> value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
+    public ExtractorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Constructs a new exception with the specified cause and a detail
+     * message of <tt>(cause==null ? null : cause.toString())</tt> (which
+     * typically contains the class and detail message of <tt>cause</tt>).
+     * This constructor is useful for exceptions that are little more than
+     * wrappers for other throwables (for example, {@link
+     * java.security.PrivilegedActionException}).
+     *
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).  (A <tt>null</tt> value is
+     *         permitted, and indicates that the cause is nonexistent or
+     *         unknown.)
+     */
+    public ExtractorException(Throwable cause) {
+        super(cause);
+    }
+}
--- Components/Extractor/src/test/java/edu/si/services/camel/extractor/ExtractorComponentTest.java
+++ Components/Extractor/src/test/java/edu/si/services/camel/extractor/ExtractorComponentTest.java
@@ -27,6 +27,7 @@
 
 package edu.si.services.camel.extractor;
 
+import org.apache.camel.CamelExecutionException;
 import org.apache.camel.Message;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
@@ -39,58 +40,67 @@ import org.rauschig.jarchivelib.ArchiveEntry;
 import org.rauschig.jarchivelib.ArchiveStream;
 import org.rauschig.jarchivelib.Archiver;
 import org.rauschig.jarchivelib.ArchiverFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
+
+/**
+ * @author jshingler
+ * @author jbirkhimer
+ */
 public class ExtractorComponentTest extends CamelTestSupport
 {
+    private static final Logger LOG = LoggerFactory.getLogger(ExtractorComponentTest.class);
+    private StringBuilder expectedExceptionMsg;
 
     //TODO: Add more test!!! Need to make sure that Producer finds the correct new folder if there are multiple folders initially
     //currently we assume that an archive contains one or no compressed directory.
     @Test
     public void testTarballExtractor() throws Exception
     {
-        testExtractor("p1d246-test-targz.tar.gz");
+        testExtractor("test-targz-with-directory.tar.gz");
     }
 
     @Test
     public void testTarExtractor() throws Exception
     {
-        testExtractor("p1d246-test-tar.tar");
+        testExtractor("test-tar-with-directory.tar");
     }
 
     @Test
     public void testZipExtractor() throws Exception
     {
-        //testExtractor("p1d246-test-zip.zip");
-        testExtractor("p1d246-test-zip.zip"); //zip with compressed directory
-        testExtractor("ECU-001-D0001-test-zip.zip"); //zip without compressed directory
-    }
-
-    @Test
-    public void testZipOverwriteWithDirectory() throws Exception
-    {
-        //zip with compressed directory
-        testExtractor("p1d246-test-zip.zip", false);
-        testExtractor("p1d246-test-zip.zip");
+        testExtractor("test-zip-without-directory.zip");
     }
 
     @Test
     public void testZipOverwriteWithoutDirectory() throws Exception
     {
         //test zip without compressed directory
-        testExtractor("ECU-001-D0001-test-zip.zip", false);
-        testExtractor("ECU-001-D0001-test-zip.zip");
+        testExtractor("test-zip-without-directory.zip", false);
+        testExtractor("test-zip-without-directory.zip");
     }
 
     @Test
     public void testOverwrite() throws Exception
     {
-        testExtractor("p1d246-test-targz.tar.gz", false);
-        testExtractor("p1d246-test-targz.tar.gz");
+        testExtractor("test-targz-with-directory.tar.gz", false);
+        testExtractor("test-targz-with-directory.tar.gz");
+    }
+
+    @Test
+    public void testWithDirException() throws Exception
+    {
+        //zip with compressed directory
+        testExtractor("test-zip-with-directory.zip");
+    }
+
+    @Test
+    public void testFileExtException() throws Exception {
+        testExtractor("test-no-file-ext");
     }
 
     public void testExtractor(String archive) throws Exception
@@ -98,58 +108,66 @@ public class ExtractorComponentTest extends CamelTestSupport
         this.testExtractor(archive, true);
     }
 
-    public void testExtractor(String archive, boolean delete) throws Exception
-    {
+    public void testExtractor(String archive, boolean delete) throws Exception {
+        File file = new File(this.getClass().getResource("/" + archive).toURI());
+
+        boolean isNoFileExtTest = file.getName().contains(".") ? false : true;
+        LOG.debug("Testing For File Ext: {}", isNoFileExtTest);
+
         MockEndpoint mock = getMockEndpoint("mock:result");
         mock.expectedMinimumMessageCount(msgCount + 1);
 
-        File file = new File(this.getClass().getResource("/" + archive).toURI());
+        File body = null;
+        try {
+            template.sendBody("direct:start", file);
 
-        int numFiles = countFilesInArchive(file);
+            Message msg = mock.getExchanges().get(msgCount++).getIn();
+            body = msg.getBody(File.class);
 
-        template.sendBody("direct:start", file);
+            int numFiles = countFilesInArchive(file);
 
-        assertMockEndpointsSatisfied();
-
-        Message msg = mock.getExchanges().get(msgCount++).getIn();
-        File body = msg.getBody(File.class);
-
-        try
-        {
             assertNotNull("Results should not be null", body);
             assertTrue("Results should be a directory", body.isDirectory());
             assertEquals("Parent directory should be 'TestCameraTrapData'", "TestCameraTrapData", body.getParentFile().getName());
             assertEquals("Directory should contain " + numFiles + " elements", numFiles, body.list().length);
 
-            log.debug("Result body is not null: {}", (body != null));
-            log.debug("Result body should be a directory: {}", body.isDirectory());
-            log.debug("Result body parent File is: {}", body.getParentFile().getName());
-            log.debug("Num of files in archive: {}, Num of files in result body: {}", numFiles, body.list().length);
-        }
-        catch (Exception ex)
-        {
+            LOG.debug("Result body is not null: {}", (body != null));
+            LOG.debug("Result body should be a directory: {}", body.isDirectory());
+            LOG.debug("Result body parent File is: {}", body.getParentFile().getName());
+            LOG.debug("Num of files in archive: {}, Num of files in result body: {}", numFiles, body.list().length);
+        } catch (CamelExecutionException e) {
+            assertTrue("Exception should be ExtractorException", e.getExchange().getException() instanceof ExtractorException);
+
+            expectedExceptionMsg = new StringBuilder();
+
+            if (isNoFileExtTest) {
+                expectedExceptionMsg.append("Improperly formatted file. No, file extension found for " + file.getName());
+            } else {
+                expectedExceptionMsg.append("Extracting archive '" + file.getName() + "' failed! ");
+                expectedExceptionMsg.append("Directory '" + getDirName(file) + "' found in archive!");
+            }
+
+            assertEquals("Exception messages are equal", expectedExceptionMsg.toString(), e.getExchange().getException().getMessage());
+
+            mock.setMinimumExpectedMessageCount(msgCount);
+            LOG.debug("Testing Exchange Failed! Exception thrown was: '{}'", e.getExchange().getException().toString());
+        } catch (Exception ex) {
             delete = true;
             throw ex;
-        }
-        finally
-        {
-            if (delete && body != null && body.isDirectory())
-            {
+        } finally {
+            if (delete && body != null && body.isDirectory()) {
                 FileUtils.deleteDirectory(body);
             }
         }
+
+        assertMockEndpointsSatisfied();
     }
 
     private int countFilesInArchive(File archiveTestFile) {
         int numFiles = 0;
-        //ZipFile zipfile = new ZipFile(file);
-
-        //numElem = (int) zipfile.stream().filter(zipEntry -> !zipEntry.isDirectory()).count();
-
-        //zipfile.close();
 
         Archiver archiver = ArchiverFactory.createArchiver(archiveTestFile);
-        log.debug("Testing Archive type: {}", archiver.getFilenameExtension());
+        LOG.debug("Testing Archive type: {}", archiver.getFilenameExtension());
 
         try (ArchiveStream archiveStream = archiver.stream(archiveTestFile)) {
             ArchiveEntry entry;
@@ -164,8 +182,28 @@ public class ExtractorComponentTest extends CamelTestSupport
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return numFiles;
+    }
+
+    public String getDirName(File archiveTestFile) {
+        String dirName = null;
+
+        Archiver archiver = ArchiverFactory.createArchiver(archiveTestFile);
+        LOG.debug("Testing Archive type: {}", archiver.getFilenameExtension());
+
+        try (ArchiveStream archiveStream = archiver.stream(archiveTestFile)) {
+            ArchiveEntry entry;
+
+            //get directory name
+            while ((entry = archiveStream.getNextEntry()) != null) {
+                if (entry.isDirectory()) {
+                    dirName = entry.getName();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dirName;
     }
 
 
@@ -203,7 +241,7 @@ public class ExtractorComponentTest extends CamelTestSupport
         }
         catch (IOException ex)
         {
-            Logger.getLogger(ExtractorComponentTest.class.getName()).log(Level.SEVERE, null, ex);
+            LOG.error(null, ex);
         }
     }
 }
