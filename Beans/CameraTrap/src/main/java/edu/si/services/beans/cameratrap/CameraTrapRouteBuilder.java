7700403fb61d35534e678699f40ca654958de33c
jbirkhimer <jbirkhimer@quotient-inc.com>
2016-06-03 16:18:01 -0400

    [SID-636] Updating Unified CT route and properties for logging in shared beans; the properties are also added to the WCS and Legacy routes.

--- Beans/CameraTrap/src/main/java/edu/si/services/beans/cameratrap/CameraTrapRouteBuilder.java
+++ Beans/CameraTrap/src/main/java/edu/si/services/beans/cameratrap/CameraTrapRouteBuilder.java
@@ -43,6 +43,10 @@ public class CameraTrapRouteBuilder extends RouteBuilder {
     @PropertyInject(value = "si.ct.id")
     static private String CT_LOG_NAME;
 
+    @PropertyInject(value = "si.ct.pipeline")
+    static private String CT_PIPELINE_NAME;
+
+
     /**
      * Configure the Camel routing rules for the Camera Trap Deployment Package Ingestion Process.
      */
@@ -54,10 +58,10 @@ public class CameraTrapRouteBuilder extends RouteBuilder {
             .routeId("CameraTrapValidatePostResourceCount")
             .choice()
                 .when(header("ResourceCount").isEqualTo(header("RelsExtResourceCount")))
-                    .log(LoggingLevel.INFO, CT_LOG_NAME, "${id} CameraTrapIngest: Post Resource Count validation passed")
+                    .log(LoggingLevel.INFO, CT_LOG_NAME, "${id} {{CT_PIPELINE_NAME}}: Post Resource Count validation passed")
                     .id("ValidatePostResourceCountWhenBlock")
                 .otherwise()
-                    .log(LoggingLevel.WARN, CT_LOG_NAME, "${id} CameraTrapIngest: Post Resource Count validation failed")
+                    .log(LoggingLevel.WARN, CT_LOG_NAME, "${id} {{CT_PIPELINE_NAME}}: Post Resource Count validation failed")
                     .to("bean:cameraTrapValidationMessage?method=createValidationMessage(${header.CamelFileParent}, 'Post Resource Count validation failed. " +
                             "Expected ${header.ResourceCount} but found ${header.RelsExtResourceCount}', false)")
                     .to("direct:validationErrorMessageAggregationStrategy")
